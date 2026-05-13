b2f55310fc879a539cbb327bc7a9a9a379792b02
nicolasmops <mopsnicolas@gmail.com>
2026-05-13 14:13:04 -0300

    Consigna Base ✅

--- main.js
+++ main.js
@@ -7,7 +7,41 @@ Distancia	  | Con SUBE registrada   | Con SUBE sin registrar
 6-12 km	    | $ 855.97		          | $ 1360.99
 12-27 km	  | $ 917.24		          | $ 1458.41
 */
+  let reg = registrada.value;
+  let km = distancia.value;
+  let precio;
 
-boton.addEventListener("click", () => {
-  // COMPLETAR
-});
+  boton.addEventListener("click", () => {
+  
+    if (reg ==="si") {
+      if (km <= 3) {
+        precio = 715.24
+      }
+      else if (km <= 6) {
+          precio = 794.74
+      }
+      else if (km <= 12) {
+          precio = 855.97
+      }
+      else if (km <= 27) {
+          precio = 917.24
+      }
+        }
+        else if(reg === "no"){
+          
+          if (km <= 3 ) {
+            precio = 1137.23
+          }
+          else if (km <= 6 ) {
+              precio = 1263.64
+          }
+          else if (km <= 12 ) {
+              precio = 1360.99
+          }
+          else if (km <= 27){
+              precio = 1458.41
+              
+          }
+        }
+        tarifa.innerText=precio;
+  });
